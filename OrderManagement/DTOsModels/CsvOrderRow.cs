5ebe6978a4c76ae75da7bf5911ad0b597032a623
Kunal Kumar <kunalkumar02034@gmail.com>
2025-09-08 11:52:22 +0530

    Optimize Import Order Function

---
+++ OrderManagement/DTOsModels/CsvOrderRow.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagement.DTOsModels
+{
+    public class CsvOrderRow
+    {
+        public string ProductId { get; set; }
+        public string UserId { get; set; }
+        public string Quantity { get; set; }
+        public string OrderDate { get; set; }
+    }
+}
